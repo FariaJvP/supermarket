c6f8d50d4eaf1fd68d97c5ca4b05a2329093530b
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-07 18:00:55 -0300

    initial commit

---
+++ src/test/java/com/sm/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
