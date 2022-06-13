5b9cecd9150098a296d9db18799cd56dbc93dfc4
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-13 01:34:08 -0300

    adds a dto to send client data to the controller

---
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/dto/NewProductRequest.java
@@ -0,0 +1,15 @@
+package com.sm.supermarket.productandinventory.web.dto;
+
+import java.math.BigDecimal;
+
+public class NewProductRequest {
+
+    private String name;
+
+    private Long brandId;
+
+    private BigDecimal price;
+
+    private String unit;
+
+}
