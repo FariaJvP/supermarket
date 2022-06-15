c0954580f23bdc6b862218b043a0459eec4f28bd
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-15 19:49:45 -0300

    adds new methods to handle exceptions that may be thrown if the client send an invalid data

--- productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ValidationHandler.java
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ValidationHandler.java
@@ -1,11 +1,14 @@
 package com.sm.supermarket.productandinventory.web.validation;
 
 import com.sm.supermarket.productandinventory.infrastructure.domainentitiesinterfacerepositories.brand.BrandNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+import java.sql.SQLException;
 import java.time.Instant;
 
 @RestControllerAdvice
@@ -17,5 +20,38 @@ public class ValidationHandler {
                 HttpStatus.NOT_FOUND.value(), "brand not found", exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseToWrongDataSentInForm> handlerForCheckConstraintViolationThrewByDatabase(SQLException exception){
+        ResponseToWrongDataSentInForm response = new ResponseToWrongDataSentInForm(Instant.now(),
+                HttpStatus.BAD_REQUEST.value(), "database check constraint violation", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseToWrongDataSentInForm> handlerForConstraintViolationThrewByBeanValidation(ConstraintViolationException exception){
+        ResponseToWrongDataSentInForm response = new ResponseToWrongDataSentInForm(Instant.now(),
+                HttpStatus.BAD_REQUEST.value(), "database constraint violation", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseToWrongDataSentInForm> handlerForConstraintDataIntegrityViolation(DataIntegrityViolationException exception){
+        ResponseToWrongDataSentInForm response = new ResponseToWrongDataSentInForm(Instant.now(),
+                HttpStatus.BAD_REQUEST.value(), "database data integrity violation", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ResponseToWrongDataSentInForm> handlerForIllegalArgument(IllegalArgumentException exception){
+        ResponseToWrongDataSentInForm response = new ResponseToWrongDataSentInForm(Instant.now(),
+                HttpStatus.BAD_REQUEST.value(), "illegal argument", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
