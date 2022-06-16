84da0d3ee98b3e84a8be977ee8a48d17e71ff263
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-16 11:10:32 -0300

    adds a new method to handle for method argument not valid exceptions which may be caused by the constraints in DTO

--- productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ValidationHandler.java
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ValidationHandler.java
@@ -4,12 +4,17 @@ import com.sm.supermarket.productandinventory.infrastructure.domainentitiesinter
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
 import java.sql.SQLException;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ValidationHandler {
@@ -53,5 +58,17 @@ public class ValidationHandler {
                 HttpStatus.BAD_REQUEST.value(), "illegal argument", exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public List<ResponseToWrongDataSentInForm> handlerForMethodArgumentNotValid(MethodArgumentNotValidException exception){
+        List<ResponseToWrongDataSentInForm> responseList = new ArrayList<>();
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(error -> {
+            ResponseToWrongDataSentInForm response = new ResponseToWrongDataSentInForm(Instant.now(), HttpStatus.BAD_REQUEST.value(), "method argument not valid", error.getDefaultMessage());
+            responseList.add(response);
+        });
+        return responseList;
     }
 }
