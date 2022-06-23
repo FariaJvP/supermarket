06b4941a73600f561861ec4a1e2df3c0b2688e48
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-22 22:30:12 -0300

    adds an entity interface to be used with the infrastructure layer

---
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/entities/inventory/purchaserequisition/history/EntityRepositoryForPurchaseRequisitionHistory.java
@@ -0,0 +1,5 @@
+package com.sm.supermarket.productandinventory.entities.inventory.purchaserequisition.history;
+
+public interface EntityRepositoryForPurchaseRequisitionHistory {
+    void sendToDatabase(PurchaseRequisitionHistory purchaseRequisitionHistory);
+}
