2401a4a71b66f8dff7c00ad1dbda39cce714e53c
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-24 07:50:10 -0300

    replaces the products position to align with quantity ordered

--- productandinventory/src/test/resources/import.sql
+++ productandinventory/src/test/resources/import.sql
@@ -1,11 +1,11 @@
 INSERT INTO tb_brand (brand_name) VALUES ('Coca-Cola'), ('Pepsi'), ('Kellogg''s'),('General Mills'),('Splendid Spoon'),('Mosaic Foods'),('Nestlé'),('Danone'),('Persil'),('Tide'),('Smucker''s'), ('PB2'), ('Dave''s Killer'),('The Hagen Group'),('White Pearl');
 
+INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('White Rice',  'Plastic bag rice with 5kg.', 5.00, 'KG', 15);
+INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('Cereal', 'Cereal box with 400g', 7.85, 'UNIT', 3);
 INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('Penne alla Vodka', 'Frozen meal, 286g, vegetarian and no added sugar.', 15.00, 'UNIT', 6);
 INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('Coca-Cola Traditional', '2 liter bottle', 1.85, 'UNIT', 1);
 INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('Coca-Cola Twist', '2 liter bottle', 1.89, 'UNIT', 1);
 INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('Coca-Cola Zero', '2 liter bottle', 2.00, 'UNIT', 1);
-INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('White Rice',  'Plastic bag rice with 5kg.', 5.00, 'KG', 15);
-INSERT INTO tb_product(product_name, product_description, price, unit, brand_id) VALUES ('Cereal', 'Cereal box with 400g', 7.85, 'UNIT', 3);
 
 INSERT INTO tb_purchase_requisition(process_status) VALUES ('WAITING_HISTORY_STATUS_UPDATE');
 INSERT INTO tb_purchase_requisition(process_status) VALUES ('WAITING_HISTORY_STATUS_UPDATE');
