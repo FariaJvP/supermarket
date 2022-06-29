58ef56806b2187136e6b856d2654ecbbb729d01f
Joao Vitor <jvfaria.paz@gmail.com>
2022-06-29 16:25:02 -0300

    adds an overload method to receive a product request

--- productandinventory/src/test/java/com/sm/supermarket/productandinventory/builders/HttpRequestBuilder.java
+++ productandinventory/src/test/java/com/sm/supermarket/productandinventory/builders/HttpRequestBuilder.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sm.supermarket.productandinventory.web.dto.NewProductRequest;
 import com.sm.supermarket.productandinventory.web.dto.PurchaseRequisitionRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,6 +21,15 @@ public class HttpRequestBuilder {
                 .content(objectMapper
                         .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
                         .writeValueAsString(purchaseRequisitionRequest));
+    }
+
+    public MockHttpServletRequestBuilder getMockHttpServletRequestBuilder(URI uri, ObjectMapper objectMapper, NewProductRequest newProductRequest) throws JsonProcessingException {
+        return MockMvcRequestBuilders
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper
+                        .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                        .writeValueAsString(newProductRequest));
     }
 
 }
