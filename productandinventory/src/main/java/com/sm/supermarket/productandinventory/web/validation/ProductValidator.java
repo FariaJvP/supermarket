949974202a61affd86f264f801dfa7fee131591c
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-01 06:48:33 -0300

    updates the validators to check for null values before validation in order to avoid null pointer exception and an illegal state of the object

--- productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ProductValidator.java
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ProductValidator.java
@@ -1,26 +1,29 @@
 package com.sm.supermarket.productandinventory.web.validation;
 
 import com.sm.supermarket.productandinventory.entities.product.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
 
+@PropertySource("classpath:messages.properties")
 public class ProductValidator implements ConstraintValidator<ValidProduct, Long> {
 
     @PersistenceContext
     private EntityManager entityManager;
 
+    @Value("${product}")
     private String message;
 
     @Override
-    public void initialize(ValidProduct constraintAnnotation) {
-        message = constraintAnnotation.message();
-    }
-
-    @Override
     public boolean isValid(Long productId, ConstraintValidatorContext context) {
+        if (Objects.isNull(productId)){
+            throw new IllegalStateException(message);
+        }
         Product product = entityManager.find(Product.class, productId);
         return product != null;
     }
--- productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ValidPackageUnitValidator.java
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/web/validation/ValidPackageUnitValidator.java
@@ -1,19 +1,29 @@
 package com.sm.supermarket.productandinventory.web.validation;
 
 import com.sm.supermarket.productandinventory.entities.product.PackageUnit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
+@PropertySource("classpath:messages.properties")
 public class ValidPackageUnitValidator implements ConstraintValidator<ValidPackageUnit, String> {
 
     private final PackageUnit[] values = PackageUnit.values();
     private final ArrayList<PackageUnit> list = new ArrayList<>(Arrays.asList(values));
 
+    @Value("${packageunit}")
+    private String errorMessage;
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (Objects.isNull(value) || value.isEmpty()){
+            throw new IllegalStateException(errorMessage);
+        }
         try {
             return list.contains(PackageUnit.valueOf(value));
         }catch (IllegalArgumentException exception){
