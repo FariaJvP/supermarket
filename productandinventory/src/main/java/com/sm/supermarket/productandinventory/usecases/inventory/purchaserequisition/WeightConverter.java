fa9765ac85b2d1ef4d4f1ab8d56f6b327c4e610c
Joao Vitor <jvfaria.paz@gmail.com>
2022-07-01 19:14:11 -0300

    refactors the weight converter to use map and avoid ciclomatic complexity

--- productandinventory/src/main/java/com/sm/supermarket/productandinventory/usecases/inventory/purchaserequisition/WeightConverter.java
+++ productandinventory/src/main/java/com/sm/supermarket/productandinventory/usecases/inventory/purchaserequisition/WeightConverter.java
@@ -4,28 +4,28 @@ import com.sm.supermarket.productandinventory.entities.product.PackageUnit;
 import com.sm.supermarket.productandinventory.web.dto.ProductToBeOrderedRequisition;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WeightConverter {
 
+    private Map<PackageUnit, BigInteger> mapOfValues = new HashMap<PackageUnit, BigInteger>(){
+        {
+            put(PackageUnit.UNIT, BigInteger.valueOf(1));
+            put(PackageUnit.MG, BigInteger.valueOf(1));
+            put(PackageUnit.KG, BigInteger.valueOf(1000));
+            put(PackageUnit.TON, BigInteger.valueOf(1000).pow(2));
+            put(PackageUnit.ML, BigInteger.valueOf(1));
+            put(PackageUnit.LT, BigInteger.valueOf(1000));
+        }};
+
     public BigInteger convert(ProductToBeOrderedRequisition productToBeOrdered){
         if ((productToBeOrdered.getReferenceUnit() != null) && (!productToBeOrdered.getReferenceUnit().isEmpty())){
-            if (productToBeOrdered.getReferenceUnit().equals(PackageUnit.UNIT.toString())){
-                return productToBeOrdered.getQuantity();
+            if(!mapOfValues.keySet().toString().contains(productToBeOrdered.getReferenceUnit())) {
+                throw new WrongConversionException("Could not convert the weight, please check for valid package units");
             }
-            if(productToBeOrdered.getReferenceUnit().equals(PackageUnit.TON.toString())) {
-                return productToBeOrdered.getQuantity().multiply(BigInteger.valueOf(1000)).pow(2);
-            }
-            if(productToBeOrdered.getReferenceUnit().equals(PackageUnit.KG.toString())){
-                return productToBeOrdered.getQuantity().multiply(BigInteger.valueOf(1000));
-            }
-            if(productToBeOrdered.getReferenceUnit().equals(PackageUnit.LT.toString())){
-                return productToBeOrdered.getQuantity().multiply(BigInteger.valueOf(1000));
-            }
+            return productToBeOrdered.getQuantity().multiply(mapOfValues.get(PackageUnit.valueOf(productToBeOrdered.getReferenceUnit())));
         }
-        else {
-            throw new IllegalStateException("illegal state for package unit");
-        }
-        throw new WrongConversionException("Could not convert the weight, please check for valid package units");
+        throw new IllegalStateException("illegal state for package unit");
     }
-
 }
